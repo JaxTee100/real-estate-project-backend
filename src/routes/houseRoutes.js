1bf28a9f1b927bea48856cce6c0d3db615d77745
Tochukwu Tobias <jaxj9449@gmail.com>
2025-06-23 02:27:53 +0100

    removed all authmiddleware for now

--- src/routes/houseRoutes.js
+++ src/routes/houseRoutes.js
@@ -7,11 +7,11 @@ import express from 'express';
 const router = express.Router();
 
 
-router.post('/create-new-house', authenticateJwt , upload.array('images', 5),createHouse);
-router.get('/get-all-houses',authenticateJwt, getAllHouses);
-router.get('/get-house/:id', authenticateJwt, getHouseByID);
-router.put('/update-house/:id', authenticateJwt, upload.array('images', 5), updateHouse); // Assuming createHouse can also handle updates
-router.delete('/delete-house/:id', authenticateJwt, deleteHouse); // Assuming you have a deleteHouse controller
+router.post('/create-new-house', upload.array('images', 5),createHouse);
+router.get('/get-all-houses',  getAllHouses);
+router.get('/get-house/:id', getHouseByID);
+router.put('/update-house/:id',  upload.array('images', 5), updateHouse); // Assuming createHouse can also handle updates
+router.delete('/delete-house/:id',  deleteHouse); // Assuming you have a deleteHouse controller
 
 router.get('/client-houses', clientHouses)
 
